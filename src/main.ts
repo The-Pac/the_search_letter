771d659be95b73cbe9ee584c28e6061305c4d6c3
Baptiste Arsac <baptiste.arsac@astree-software.fr>
2023-03-03 16:18:25 +0100

    next finished

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,15 @@ import {createApp} from 'vue'
 import App from './app/App.vue'
 import './app/bootstrap.scss'
 import {addIcons, OhVueIcon} from "oh-vue-icons";
-import {CoArrowThickToBottom, CoArrowThickToTop, CoExitToApp, CoMediaPlay} from "oh-vue-icons/icons/co";
-import {BiPeopleFill} from "oh-vue-icons/icons/bi";
-import {FaRegularWindowMaximize, FaRegularWindowMinimize} from "oh-vue-icons/icons/fa";
-
 import router from "./app/Router";
+import {RiLoader5Line} from "oh-vue-icons/icons/ri";
 
 const app = createApp(App)
 
 
 //ICONS
 addIcons(
-    CoMediaPlay,
-    BiPeopleFill,
-    CoExitToApp,
-    FaRegularWindowMaximize,
-    FaRegularWindowMinimize,
-    CoArrowThickToBottom,
-    CoArrowThickToTop
+    RiLoader5Line
 )
 app.component("v-icon", OhVueIcon);
 
